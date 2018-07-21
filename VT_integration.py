731646de85d9a38bd971a28905e110aafb5fffe3
jemindesai <jemindesai@yahoo.com>
2018-07-21 15:25:32 -0700

    Add files via upload

--- VT_integration.py
+++ VT_integration.py
@@ -8,25 +8,26 @@ class VirusTotal(AppIntegration):
 		AppIntegration.__init__(self)
 		self.api_key = self.secrets['virus_total']['api_key']
 
-	def scan_and_report(self, resource):
-		"""Takes in a string RESOURCE, which is the url of the file 
-		in question and tries to retrieve that url's report. If the 
-		report does not exist, it will scan the url and wait for a 
-		report to be generated before returning the response dictionary.
+	def scan_and_report(self, d):
+		"""Takes in a dict D with key RESOURCE, which is (a string) 
+		the url of the file in question and tries to retrieve that 
+		url's report. If the report does not exist, it will scan the 
+		url and wait for a report to be generated before returning 
+		the response dictionary.
 		"""
 
 		url = 'https://www.virustotal.com/vtapi/v2/url/report'
-		params = {'apikey': self.api_key, 'resource': resource}
+		params = {'apikey': self.api_key, 'resource': d['resource']}
 		response = requests.get(url, params=params).json()
 		
 		if 'scans' not in response:
 			
 			url = 'https://www.virustotal.com/vtapi/v2/url/scan'
-			params = {'apikey': self.api_key, 'url': resource}
+			params = {'apikey': self.api_key, 'url': d['resource']}
 			response = requests.post(url, data=params).json()
 
 			url = 'https://www.virustotal.com/vtapi/v2/url/report'
-			params = {'apikey': self.api_key, 'resource': resource}
+			params = {'apikey': self.api_key, 'resource': d['resource']}
 
 			start = time.time()
 			interval = 20
@@ -41,22 +42,22 @@ class VirusTotal(AppIntegration):
 		else:
 			return response
 
-	def get_domain_report(self, domain):
+	def get_domain_report(self, d):
 		"""
-		Takes in a string DOMAIN and outputs a dictionary RESPONSE
-		containing a report.
+		Takes in a dict D with key DOMAIN (as a string) and outputs 
+		a dictionary RESPONSE containing a report.
 		"""
 		url = 'https://www.virustotal.com/vtapi/v2/domain/report'
-		params = {'apikey': self.api_key, 'domain': domain}
+		params = {'apikey': self.api_key, 'domain': d['domain']}
 		response = requests.get(url, params=params)
 		return response.json()
 
-	def get_ip_report(self, ip):
+	def get_ip_report(self, d):
 		"""
-		Takes in a string IP and outputs a dictionary RESPONSE
-		containing a report.
+		Takes in a dict D with key IP (as a string) and outputs a 
+		dictionary RESPONSE containing a report.
 		"""
 		url = 'https://www.virustotal.com/vtapi/v2/ip-address/report'
-		params = {'apikey': self.api_key, 'ip': ip}
+		params = {'apikey': self.api_key, 'ip': d['ip']}
 		response = requests.get(url, params=params)
 		return response.json()
