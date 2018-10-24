f3e4faa1436f62b7b8ac0651271d6acccc5c782c
Veeral Patel <veeralpatel4@gmail.com>
2018-10-23 17:48:20 -0700

    created xfe_integration.py

---
+++ xfe_integration.py
@@ -0,0 +1,45 @@
+from appintegration import AppIntegration
+import requests
+import base64
+
+# TODO: get `lookup_ip` to work
+# TODO: get `lookup_hash` to work
+# TODO: remove print from `lookup_hash`
+
+# Stands for IBM XForce Exchange
+class XFE(AppIntegration):
+    def __init__(self):
+        AppIntegration.__init__(self)
+        api_key = self.secrets['xfe']['api_key']
+        api_password = self.secrets['xfe']['api_password']
+
+        # we must pass base64(apikey:pass) in the Authorization header
+        string_to_encode = '%s:%s' % (api_key, api_password)
+        self.token = base64.b64encode(string_to_encode.encode())
+
+    def lookup_ip(self, d):
+        """Dictionary d has required key, `ip`."""
+        try:
+            ip = d['ip']
+        except KeyError:
+            return { 'status': 'Error', 'error_message': 'Key ip is required'} 
+
+        return requests.get("https://api.xforce.ibmcloud.com/ipr/%s" % ip, headers={
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Basic %s' % self.token.decode()
+        }).json()
+
+    def lookup_hash(self, d):
+        """Dictionary d has required key, `hash`. Can be any type of hash (I believe,
+        documentation doesn't specify.)"""
+        try:
+            hash = d['hash']
+        except KeyError:
+            return { 'status': 'Error', 'error_message': 'Key hash is required'} 
+        
+        return requests.get("https://api.xforce.ibmcloud.com/malware/%s" % hash, headers={
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Basic %s' % self.token.decode()
+        }).json()
